7ea0087ac3f76890252d27fffd2d929ee4419ba2
phasenraum2010 <thomas@woehlke.org>
2018-06-24 01:28:34 +0200

    working on #6

--- src/main/java/org/woehlke/neo4j/example/config/ContextClosedListener.java
+++ src/main/java/org/woehlke/neo4j/example/config/ContextClosedListener.java
@@ -1,5 +1,6 @@
 package org.woehlke.neo4j.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.woehlke.neo4j.example.config.helper.LoggingComponentImpl;
  */
 @Component
 public class ContextClosedListener  extends LoggingComponentImpl implements ApplicationListener<ContextClosedEvent> {
+
 
     @Override
     protected String[] getLogInfos() {
@@ -25,5 +27,17 @@ public class ContextClosedListener  extends LoggingComponentImpl implements Appl
     @Override
     public void onApplicationEvent(ContextClosedEvent event) {
         this.logInfo();
+        this.allProperties.logDebug();
+        this.storageProperties.logDebug();
+    }
+
+    private final AllProperties allProperties;
+
+    private final StorageProperties storageProperties;
+
+    @Autowired
+    public ContextClosedListener(AllProperties allProperties, StorageProperties storageProperties) {
+        this.allProperties = allProperties;
+        this.storageProperties = storageProperties;
     }
 }
